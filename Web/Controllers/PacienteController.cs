c930c652d053378952d50b76b557b44799e5b58f
Karla Pereira <karlapereira@gec.inatel.br>
2023-03-25 15:48:36 -0300

    feat: Interface Web

---
+++ Web/Controllers/PacienteController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExemploMaterializacao.Controllers
+{
+  public class PacienteController : Controller
+  {
+    // GET: Paciente
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    public ActionResult ListaPacientes()
+    {
+      ServicoExposto.ServicoSoapClient servico = new ServicoExposto.ServicoSoapClient();
+      
+      return View(servico.GetMeusPacientes());
+    }
+
+  }
+}
