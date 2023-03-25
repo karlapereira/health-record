30294a50fbf52356b4fe5de725c40ef83652fee6
Karla Pereira <karlapereira@gec.inatel.br>
2023-03-25 11:43:02 -0300

    feat: Services layer

---
+++ Servico/Servico.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="Servico.asmx.cs" Class="Servico.Servico" %>
---
+++ Servico/Servico.asmx.cs
@@ -0,0 +1,29 @@
+﻿using Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using TO;
+
+namespace Servico
+{
+  /// <summary>
+  /// Descrição resumida de Servico
+  /// </summary>
+  [WebService(Namespace = "http://tempuri.org/")]
+  [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+  [System.ComponentModel.ToolboxItem(false)]
+  // Para permitir que esse serviço da web seja chamado a partir do script, usando ASP.NET AJAX, remova os comentários da linha a seguir. 
+  // [System.Web.Script.Services.ScriptService]
+  public class Servico : System.Web.Services.WebService
+  {
+
+    [WebMethod]
+    public List<PacienteTO> GetMeusPacientes()
+    {
+      PacienteNegocio negPaciente = new PacienteNegocio();
+      return negPaciente.GetMeusPacientes();
+    }
+  }
+}
